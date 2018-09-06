f86dd0eb2eee8ccf8b89af8b5e1f1b61c10892f6
markkvdb <markvanderbroek@gmail.com>
2018-09-06 09:26:46 +0200

    Initial commit

---
+++ run.R
@@ -0,0 +1,15 @@
+# Author:   markvanderbroek@gmail.com
+# Date:     06-09-2018
+# Purpose:  Exploratory analysis of the bike sharing system of Boston
+
+# Load libraries
+library(data.table)
+library(tidyverse)
+library(leaflet)
+library(leaflet.extras)
+library(geosphere)
+library(lubridate)
+library(gridExtra)
+library(grid)
+library(psych)
+
